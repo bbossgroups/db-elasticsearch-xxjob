bb128caa8dda986c6a7507aef50e7784561edb43
yinbp <yin-bp@163.com>
2019-04-21 00:58:31 +0800

    提交xxjob作业代码和配置文件
    提交xxjob作业代码和配置文件

---
+++ src/test/java/org/frameworkset/elasticsearch/imp/ApplicationTest.java
@@ -0,0 +1,19 @@
+package org.frameworkset.elasticsearch.imp;
+
+import org.frameworkset.elasticsearch.client.schedule.xxjob.XXJobApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author xuxueli 2018-10-31 19:05:43
+ */
+public class ApplicationTest {
+    private static Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
+
+    public static void main(String[] args) {
+
+        XXJobApplication.main(args);
+
+    }
+
+}
